e2a24d0fa6a775033c04cf909c6dca91569d3b4d
Luis <yepesluis006@gmail.com>
2025-08-21 12:44:59 -0500

    actualizacion-protecto1.1

--- js/about.js
+++ js/about.js
@@ -51,3 +51,6 @@ function goProyectos() {
 
 
 document.addEventListener("DOMContentLoaded", goProyectos());
+
+
+
--- js/proyecto.js
+++ js/proyecto.js
@@ -13,3 +13,71 @@ function goAbout() {
 }
 
 document.addEventListener("DOMContentLoaded", goAbout());
+
+
+function redidirigirToRepository(btn, url) {
+
+    btn = document.querySelector("." + btn);
+    btn.style.cursor = "pointer";
+
+
+    btn.addEventListener("click", (e) => {
+
+        window.open(url, "_blank")
+
+    })
+
+}
+
+
+function redirigirYoutubbeAzul() {
+
+    redidirigirToRepository("btn-repositorio-youtube-azul", "https://github.com/LuisYepes601/youtube-azul");
+
+
+}
+
+document.addEventListener("DOMContentLoaded", redirigirYoutubbeAzul());
+
+
+
+
+function redidirigirToDemo(btn, url) {
+
+    btn = document.querySelector("." + btn);
+    btn.style.cursor = "pointer";
+
+
+    btn.addEventListener("click", (e) => {
+
+        window.open(url, "_blank")
+
+    })
+
+}
+
+
+function redidirigirToDemoYoutoubeAzul() {
+
+    redidirigirToDemo("btn-demo-youtube", "https://luisyepes601.github.io/youtube-azul/");
+}
+
+document.addEventListener("DOMContentLoaded", redidirigirToDemoYoutoubeAzul());
+
+
+
+function redidirigirToDemoEncriptador() {
+
+    redidirigirToRepository("btn-repositorio-encriptador", "https://github.com/LuisYepes601/Proyecto-alura.git");
+}
+
+document.addEventListener("DOMContentLoaded", redidirigirToDemoEncriptador());
+
+
+
+function redidirigirToDemoEnvcriptador() {
+
+    redidirigirToDemo("btn-demo-encriptador", "https://luisyepes601.github.io/Proyecto-alura/");
+}
+
+document.addEventListener("DOMContentLoaded", redidirigirToDemoEnvcriptador());
