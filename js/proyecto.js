c72115f782c65b4b94fa06b87e14dd20a371f896
LuisYepes601 <yepesluis006@gmail.com>
2025-08-28 17:59:25 -0500

    actualizacion-protecto1.4

--- js/proyecto.js
+++ js/proyecto.js
@@ -81,3 +81,47 @@ function redidirigirToDemoEnvcriptador() {
 }
 
 document.addEventListener("DOMContentLoaded", redidirigirToDemoEnvcriptador());
+
+function goProyectos() {
+
+    const ver_mis_proyectos = document.querySelectorAll(".seccion-proyectos");
+    if (ver_mis_proyectos.length > -1) {
+
+        console.log(ver_mis_proyectos);
+
+        ver_mis_proyectos.forEach(btn => {
+
+            btn.addEventListener("click", (e) => {
+                e.preventDefault();
+                window.location.href = "proyectos.html";
+            })
+        });
+
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", goProyectos());
+
+function goAbout() {
+
+    const btn_sobre_mi = document.querySelectorAll(".btn-sobre-mi");
+
+
+    if (btn_sobre_mi.length > -1) {
+
+        btn_sobre_mi.forEach(btn_sobre_mi => {
+
+            btn_sobre_mi.addEventListener("click", (e) => {
+
+                e.preventDefault();
+                window.location.href = "about.html";
+
+            })
+        });
+
+    }
+
+
+
+}
